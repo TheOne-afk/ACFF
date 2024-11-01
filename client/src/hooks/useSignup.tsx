16d21dd672c1a6ee8601df07c9b10205a807dbeb
TheOne-afk <156886567+TheOne-afk@users.noreply.github.com>
2024-11-01 21:07:03 +0800

    Update useSignup.tsx

--- client/src/hooks/useSignup.tsx
+++ client/src/hooks/useSignup.tsx
@@ -16,7 +16,7 @@ export const useSignup = () => {
         for local website use this fetch: /api/user/register
         */
 
-        const response = await fetch('/api/user/register', {
+        const response = await fetch('https://acff-api.vercel.app/api/user/register', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({username,email,password}) // convert to json file
