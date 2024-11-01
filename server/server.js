cff996d744ea3f8ffceb73ff7cab31bf33522bad
TheOne-afk <156886567+TheOne-afk@users.noreply.github.com>
2024-11-01 19:06:26 +0800

    UPDATE

--- server/server.js
+++ server/server.js
@@ -19,20 +19,6 @@ app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.use('/',require('./routes/root'))
 
-// 404 not found
-app.all('*', (req,res) => {
-    res.status(404)
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    }
-    else if(req.accepts('json')){
-        res.json({message: "404 Not Found"})
-    }
-    else{
-        res.type('txt').send('404 Not Found')
-    }
-})
-
 // Example user route
 router.get('/', (req, res) => {
     res.json({message: "HELLO"});
@@ -59,4 +45,20 @@ mongoose.connect('mongodb+srv://dawan:dawan12345678@acffdb.eitoc.mongodb.net/?re
 })
 .catch((error)=>{
     console.log(error)
+})
+
+
+
+// 404 not found
+app.all('*', (req,res) => {
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }
+    else if(req.accepts('json')){
+        res.json({message: "404 Not Found"})
+    }
+    else{
+        res.type('txt').send('404 Not Found')
+    }
 })
