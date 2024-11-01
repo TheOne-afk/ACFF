52810ee253f8a2f7800e8ef785d0085e3491a6cd
TheOne-afk <156886567+TheOne-afk@users.noreply.github.com>
2024-11-01 20:21:02 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/user')
-const router = express.Router()
 const cors = require('cors')
-const path = require('path')
 
 // epxress app
 const app = express()
